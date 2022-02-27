253b5e5ee37206cc41fea6f16214b1bc21623b80
TangZhiye <372424019@qq.com>
2022-02-27 09:52:11 +0800

    'lab4'

--- lab4/chatbot.py
+++ lab4/chatbot.py
@@ -29,6 +29,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_command))
 
 
     # To start the bot:
@@ -60,6 +61,12 @@ def add(update: Update, context: CallbackContext) -> None:
         update.message.reply_text('You have said ' + msg +  ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
+
+def hello_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /hello is issued."""
+    logging.info(context.args[0])
+    msg = context.args[0]
+    update.message.reply_text('Good day, ' + msg + '!')
 
 
 
